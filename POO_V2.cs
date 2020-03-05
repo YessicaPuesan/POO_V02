aec0078b35bda7238a10bb1a1a26842440a14cbd
YessicaPuesan <61301046+YessicaPuesan@users.noreply.github.com>
2020-03-04 23:52:30 -0400

    Add files via upload

---
+++ POO_V2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_POO_v02
+{
+    class Apartamento
+    {
+        static void Main(string[] args)
+        {
+            Apartamento edificio = new Apartamento();
+            bool seguir = true;
+            string entrada = "";
+
+            while (seguir)
+            {
+                Console.Clear();
+                
+                Console.WriteLine("Elija la opcion que desee: ");
+                Console.WriteLine("1 Datos apartamento ");
+                Console.WriteLine("2 Vender Apartamento ");
+                Console.WriteLine("3 Alquilar apartamento");
+                entrada = Console.ReadLine();
+                switch (entrada)
+                {
+
+                    case "1":
+                        Console.Clear();
+
+                        Console.WriteLine("Agregar datos de Apartamento: ");
+                        Console.WriteLine("Direccion: ");
+                        edificio.Direccion = Console.ReadLine();
+                        Console.WriteLine("Color: ");
+                        edificio.Color = Console.ReadLine();
+                        Console.WriteLine("Area de construccion: ");
+                       edificio.ConstruccionArea = Console.ReadLine();
+                        Console.WriteLine("Parqueo: ");
+                       edificio.Garage = Console.ReadLine();
+                        Console.WriteLine("Habitaciones: ");
+                        edificio.Habitaciones = Console.ReadLine();
+                        Console.WriteLine("Baños: ");
+                        edificio.Baños = Console.ReadLine();
+                        edificio.Imprimir();
+                      
+                        break;
+
+                    case "2":
+
+                        Console.Clear();
+                        edificio.Sell();
+                        break;
+
+                    case "3":
+                        Console.Clear();
+
+                        edificio.Rent();
+                        break;
+                    
+
+                    default:
+                        Console.WriteLine("Algo anda mal :( elija la opcion correcta :)");
+                        Console.ReadLine();
+                        break;
+                     
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+
+}
